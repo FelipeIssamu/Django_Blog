e647c1488152791283e362030ddcf70a53a49be9
Felipe Issamu Kuratomi <issamukuratomi@gmail.com>
2024-08-19 13:09:41 -0400

    Adicionado model Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugify_new
 # Create your models here.
@@ -42,7 +43,7 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_new(self.name)
+            self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
@@ -70,8 +71,64 @@ class Page(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify_new(self.title)
+            self.slug = slugify_new(self.title, 4)
         return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,
+        dafault="",
+        null=False,
+        blank=True,
+        max_length=255
+    )
+    exerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado para o post ser exibido publicamente.'
+        ),
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True,
+                              default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Se marcado, exibirá a capa dentro do post.',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
